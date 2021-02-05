ec7672e0c0d02f35230b45ca1a6cac15bd686997
immu0001 <imran.malik01@icloud.com>
2021-02-05 16:53:21 +0530

    first commit

---
+++ api.py
@@ -0,0 +1,35 @@
+from flask import Flask
+from flask_restful import Api, Resource
+from pymongo import MongoClient
+
+client = MongoClient('mongodb+srv://Imran:IMimran123@task-pro-cluster.ucykv.mongodb.net/La_Tarea?retryWrites=true&w=majority')
+
+db = client.La_Tarea
+col = db.Tarea_Col
+cursor = col.find({})
+
+
+app = Flask(__name__)
+api = Api(app)
+
+class LastOne(Resource):
+    def get(self):
+        return str(col.find_one())
+
+
+class AllDocs(Resource):
+
+    def get(self):
+        all = ""
+        for document in col.find({}):
+            all = all + str(document)
+        return all
+
+
+
+api.add_resource(LastOne, "/LastOne")
+api.add_resource(AllDocs, "/AllDocs")
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
